3297d1682f97a638a3d70a6eb331f8e5ec1ab65b
atnguyen1 <atnguyen@gmail.com>
2023-12-04 22:35:03 -0800

    Day 5 part 1

---
+++ Day5/Day5.py
@@ -0,0 +1,55 @@
+import re
+import sys
+
+with open('5.test.txt', 'r') as fh:
+    data = fh.read().split('\n\n')
+
+    seeds = [int(x) for x in data[0].split(':')[1].split()]
+
+    maps = []
+
+
+    for d in data[1:]:
+        name, ran = d.split(':\n')
+        namem = re.match('\w+-to-(\w+).+', name)
+        name = namem.group(1)
+
+        total_ranges = []
+
+        for r in ran.split('\n'):
+            r = [int(x) for x in r.split()]
+            total_ranges.append(r)
+
+
+        maps.append((name, total_ranges))
+
+    results = []
+    for s in seeds:
+        key = s
+        res = [('Seed', s)]
+
+        for m in maps:
+            ran = m[1]
+            new_key = key
+
+            for r in ran:
+                if r[1] <= key and key <= r[1] + r[2]:      # correct range
+                    new_key = r[0] + (key - r[1])
+
+            res.append((m[0], new_key))
+            key = new_key
+
+        results.append(res)
+
+    results = sorted(results, key=lambda x: x[-1][1])
+
+    print('Part1:', results[0][-1][1])
+
+    # Part 2 work backwards
+    for m in maps[::-1]:
+        for ran in m[1]:
+            dest, start, step = ran
+            print(dest, start, step)
+
+        print(m)
+        sys.exit()
